c1ac44a0624553928e54eb26191cef14cf2c9660
V. Kurakin <valentin.kurakin@ontarget-group.com>
2021-05-04 14:06:20 +0400

    Payment Unit Tests implemented

--- transactId-library-ios/transactId-library-ios/Extensions/String+Extension.swift
+++ transactId-library-ios/transactId-library-ios/Extensions/String+Extension.swift
@@ -9,12 +9,12 @@ import Foundation
 
 extension String {
     
-    func toByteString() -> Data {
+    public func toByteString() -> Data {
         let byteArray = [UInt8](self.utf8)
         return Data(byteArray)
     }
     
-    func base64() -> String? {
+    public func base64() -> String? {
         return self.data(using: .utf8)?.base64EncodedString()
     }
 }
